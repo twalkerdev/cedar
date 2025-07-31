d4852a24573dbf146d117c8159ad7577ad76eef1
twalkerdev <tyler.walker.dev@outlook.com>
2025-07-31 17:13:30 -0400

    adds solution for move zeroes

--- src/two-pointers/move-zeroes/move-zeroes.ts
+++ src/two-pointers/move-zeroes/move-zeroes.ts
@@ -1,3 +1,21 @@
 export default function moveZeroes(nums: number[]) {
+  let left = 0;
+  let right = 1;
+
+  if (nums.length < 2) return nums;
+
+  while (right < nums.length) {
+    if (nums[left] === 0 && nums[right] !== 0) {
+      nums[left] = nums[right];
+      nums[right] = 0;
+      left++;
+      right++;
+    } else if (nums[right] === 0 && nums[left] !== 0) {
+      left++;
+      right = left + 1;
+    } else {
+      right++;
+    }
+  }
   return nums;
 }
